b009681174211ab42b03dcd46e4a66c69dacf040
Audrey Roemer <audreyroemer@gmail.com>
2018-09-16 18:24:33 +0200

    Initial Commit

---
+++ customLoader/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  customLoader
+//
+//  Created by Audrey Roemer on 16/09/2018.
+//  Copyright © 2018 Audrey Roemer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
